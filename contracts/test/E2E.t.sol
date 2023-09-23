f36b1152970b0ffcc3f9aadaac648fecd73a4203
sujithsomraaj <somraajsujith@gmail.com>
2023-09-23 10:56:42 -0400

    init: create basic test suite

--- contracts/test/E2E.t.sol
+++ contracts/test/E2E.t.sol
@@ -3,4 +3,24 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 
-contract E2ETest is Test {}
+contract E2ETest is Test {
+    uint256 EXECUTION_CHAIN_FORK_ID;
+    uint256 STORAGE_CHAIN_FORK_ID;
+
+    string constant STORAGE_CHAIN_AXELAR_CHAIN_ID = "ethereum-2";
+    uint32 constant STORAGE_CHAIN_HYPERLANE_CHAIN_ID = 5;
+
+    string constant EXECUTION_CHAIN_AXELAR_CHAIN_ID = "polygon";
+    uint32 constant EXECUTION_CHAIN_HYPERLANE_CHAIN_ID = 80001;
+
+    function setUp() external {
+        STORAGE_CHAIN_FORK_ID = vm.createSelectFork(vm.envString("STORAGE_CHAIN_RPC"));
+        EXECUTION_CHAIN_FORK_ID = vm.createSelectFork(vm.envString("EXECUTION_CHAIN_RPC"));
+
+        console.log("Created Forks", STORAGE_CHAIN_FORK_ID, EXECUTION_CHAIN_FORK_ID);
+
+        
+    }
+
+    function test_e2e() external {}
+}
