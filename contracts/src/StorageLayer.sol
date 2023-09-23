8f522c8ffb16e8edcae002ca686c2ca400cde56d
sujithsomraaj <somraajsujith@gmail.com>
2023-09-23 11:46:23 -0400

    chore: remove console

--- contracts/src/StorageLayer.sol
+++ contracts/src/StorageLayer.sol
@@ -6,8 +6,6 @@ import "./axelar/IAxelarExecutable.sol";
 
 import "./types/DataTypes.sol";
 
-import "forge-std/console.sol";
-
 contract StorageLayer is IHyperlaneRecipient, IAxelarExecutable {
     /// @dev maps storage state to a storage slot
     mapping(bytes32 storageSlot_ => StorageState) public state;
@@ -26,7 +24,6 @@ contract StorageLayer is IHyperlaneRecipient, IAxelarExecutable {
 
     /// @dev functions to receive message from remote execution layers
     function handle(uint32 _origin, bytes32 _sender, bytes calldata _message) external override {
-        console.log("inga varala 0");
         _validateAndProcess(_message);
     }
 
@@ -36,24 +33,15 @@ contract StorageLayer is IHyperlaneRecipient, IAxelarExecutable {
         string calldata sourceAddress,
         bytes calldata payload
     ) external override {
-        console.log("inga varala 0");
         _validateAndProcess(payload);
     }
 
     function _validateAndProcess(bytes memory data_) internal {
         (bytes32 selector, uint256 originChain, uint256 originChainPayloadId, bytes memory state_) =
             abi.decode(data_, (bytes32, uint256, uint256, bytes));
-        console.log("inga varala 1");
-        console.logBytes32(selector);
-        console.log(originChain);
-        console.log(originChainPayloadId);
-        console.logBytes32(keccak256("STORE_SELECTOR"));
         if (selector == keccak256("STORE_SELECTOR")) {
-            console.log("entered");
             if (!isDuplicate[originChain][originChainPayloadId]) {
-                console.log("entered again");
                 bytes32 slot = keccak256(abi.encode(++packetCounter, data_));
-                console.logBytes32(slot);
                 state[slot] = abi.decode(state_, (StorageState));
 
                 isDuplicate[originChain][originChainPayloadId] = true;
