6167dcf41a091a78d392a53eb192c9e069135d5d
sujithsomraaj <somraajsujith@gmail.com>
2023-09-23 08:24:44 -0400

    chore: add contracts skeleton

---
+++ contracts/src/ExecutionLayer.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract ExecutionLayer {
+    /// @dev allows anyone on the execution layer to store a new id on the storage layer
+    /// @param extInfo_ ipfs hash of the info
+    /// @param state_ could be any random state of this info initially
+    /// @param accesslist_ is the addresses that can acess this id on a remote chain
+    /// @param allowChainIds_ is the allowlist of chain ids [optional]
+    function initializeStorage(
+        bytes memory extInfo_,
+        uint256 state_,
+        address[] memory accesslist_,
+        uint256[] memory allowChainIds_
+    ) external {}
+
+    /// @dev allows someone to create their own version of the id in the storage
+    /// @param id_ is the slot of the storage to update on the storage layer
+    /// @dev id_ is generated at the point of storage during execution
+    /// @param state_ could be any random state of this info initially
+    function updateStorage(bytes32 id_, uint256 state_) external {}
+}
---
+++ contracts/src/StorageLayer.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract StorageLayer {
+    /// @dev stores a remote chain info
+    struct StorageState {
+        bytes extInfo_;
+        uint256 state_;
+        address[] accessList_;
+        uint256[] allowedChainIds;
+    }
+
+    /// @dev maps storage state to a storage slot
+    mapping(bytes32 storageSlot_ => StorageState) public state;
+
+    /// @dev is emitted when a new state is available at the storage layer
+    event InitData(bytes32 slotId, bytes data, uint256 currState_);
+
+    /// @dev is emitted when a new state is updated at the storage layer
+    event UpdateData(bytes32 slotId, bytes data, uint256 currState_);
+}
