b115b6442768588bfad89d2d04e081efd3cf2ba1
sujithsomraaj <somraajsujith@gmail.com>
2023-09-24 02:58:18 -0400

    chore: add button loaders

--- frontend/components/buttons/TransferButton.js
+++ frontend/components/buttons/TransferButton.js
@@ -1,8 +1,12 @@
 import { useContractWrite, useWaitForTransaction } from "wagmi";
 import StatelessERC20_ABI from "../../abi/StatelessERC20.json";
 import {parseEther} from "ethers";
+import { PuffLoader } from "react-spinners";
+import { useState } from "react";
 
 export default function TransferButton(props) {
+    const [loading, setLoading] = useState(false);
+
     const { data, write } = useContractWrite({ 
         address: '0x9fCA34a10585FE0F59bC3F7fa26888213A3A3140',
         abi: StatelessERC20_ABI,
@@ -11,14 +15,24 @@ export default function TransferButton(props) {
             props.to,
             props.amount
         ],
-        value: parseEther("0.05")
+        value: parseEther("0.05"),
      });
     
     useWaitForTransaction({ hash: data?.hash, onSuccess(data) { 
-        console.log("SUCCESS");
+        setLoading(false);
      } });
 
     return(
-        <button className="pl-20 pr-20 pt-4 pb-4 bg-blue rounded-lg mt-8 text-white font-medium" onClick={() => write?.()}>TRANSFER BUTTON</button>
+        <button className="pl-20 pr-20 pt-4 pb-4 bg-blue rounded-lg mt-8 text-white font-medium" onClick={() => {
+            setLoading(true);
+            write?.();
+        }}>
+            {
+            loading ? 
+            <div className="grid grid-cols-6 place-items-center justify-center"><PuffLoader className="col-span-1 text-white" size={20} /> <p className="col-span-5">GO TO WALLET...</p> </div>
+            :
+            "TRANSFER TOKENS"
+            }
+        </button>
     )
 }
